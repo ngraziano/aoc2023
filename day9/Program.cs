8c5c92b2fd900986ae8bc643f139e9c2cd01d9b6
Nicolas Graziano <nicolas.graziano@gmail.com>
2023-12-10 17:44:25 +0100

    Day 9

---
+++ day9/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+var result =
+    File.ReadLines("input.txt").Select(line =>
+    {
+        var sensorValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToList();
+
+        var nextValue = 0;
+        while (!sensorValues.All(v => v == 0) && sensorValues.Count > 1)
+        {
+            for (int i = 0; i < sensorValues.Count - 1; i++)
+            {
+                sensorValues[i] = sensorValues[i + 1] - sensorValues[i];
+            }
+            nextValue += sensorValues[^1];
+            sensorValues.RemoveAt(sensorValues.Count - 1);
+        }
+        return nextValue;
+    }).Sum();
+
+Console.WriteLine($"Part 1 {result}");
+
+var result2 =
+    File.ReadLines("input.txt").Select(line =>
+    {
+        var sensorValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToList();
+        var nextValue = 0;
+        int mul = 1;
+        while (!sensorValues.All(v => v == 0) && sensorValues.Count > 1)
+        {
+            nextValue += mul*sensorValues[0];
+            mul *= -1;
+            for (int i = 0; i < sensorValues.Count - 1; i++)
+            {
+                sensorValues[i] = sensorValues[i + 1] - sensorValues[i];
+            }
+            sensorValues.RemoveAt(sensorValues.Count - 1);
+        }
+        return nextValue;
+    }).Sum();
+
+Console.WriteLine($"Part 2 {result2}");
