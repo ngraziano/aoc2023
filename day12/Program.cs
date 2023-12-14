31d45a7d9534db2975ec45b46ecb8b17bdf4f20a
Nicolas Graziano <nicolas.graziano@gmail.com>
2023-12-14 11:36:34 +0100

    day12 Improvement

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,28 +1,31 @@
 ﻿
-using day12;
-using System;
 using System.Diagnostics;
-using System.Reflection;
 
-static long NbMatch(Span<char> condition, int firstGroup, IEnumerable<int> conditionGroups, bool wasInGroup, Stats stats)
+static long NbMatchI(Span<char> condition, int firstGroup, IEnumerable<int> conditionGroups, bool wasInGroup)
 {
-    if (!stats.CheckStat())
+    return NbMatchC(condition, firstGroup, conditionGroups, wasInGroup, new Dictionary<(string, int, int, bool), long>());
+}
+
+static long NbMatchC(Span<char> condition, int firstGroup, IEnumerable<int> conditionGroups, bool wasInGroup, Dictionary<(string, int, int, bool), long> cache)
+{
+    var key = (condition.ToString(), firstGroup, conditionGroups.Aggregate(0, (agg, val) => (agg * 10) + val), wasInGroup);
+    if (cache.TryGetValue(key, out var value))
     {
-        return 0;
+        return value;
     }
 
     if (condition.Length == 0)
     {
-        return (!conditionGroups.Any() || (conditionGroups.Last() == 0)) ? 1 : 0;
+        return (firstGroup == 0 && !conditionGroups.Any()) ? 1 : 0;
     }
-
+    
     var path1 = 0L;
     var currentState = condition[0];
     if (currentState == '.' || currentState == '?')
     {
         if (!wasInGroup)
         {
-            path1 = NbMatch(condition[1..], firstGroup, conditionGroups, false, currentState == '.' ? stats : stats.DecPassed());
+            path1 = NbMatchC(condition[1..], firstGroup, conditionGroups, false, cache);
         }
         else if (firstGroup > 0)
         {
@@ -30,7 +33,7 @@ static long NbMatch(Span<char> condition, int firstGroup, IEnumerable<int> condi
         }
         else
         {
-            path1 = NbMatch(condition[1..], conditionGroups.FirstOrDefault(0), conditionGroups.Skip(1), false, currentState == '.' ? stats : stats.DecPassed());
+            path1 = NbMatchC(condition[1..], conditionGroups.FirstOrDefault(0), conditionGroups.Skip(1), false, cache);
 
         }
     }
@@ -43,39 +46,41 @@ static long NbMatch(Span<char> condition, int firstGroup, IEnumerable<int> condi
         }
         else
         {
-            path2 = NbMatch(condition[1..], firstGroup - 1, conditionGroups, true, currentState == '#' ? stats : stats.DecFailed());
-
+            path2 = NbMatchC(condition[1..], firstGroup - 1, conditionGroups, true, cache);
         }
     }
-    return path1 + path2;
+
+    var result = path1 + path2;
+
+    cache[key] = result;
+    return result;
 }
 
-#if METHOD_NAIVE_TOO_SLOW
+#if true
 
 var sumNb = File.ReadLines("input.txt")
-    .Select((line,index) =>
+    .Select((line, index) =>
 {
     var stopwatch = Stopwatch.StartNew();
     var linePart = line.Split(' ');
     var condition = linePart[0];
     var conditionGroups = linePart[1].Split(",").Select(int.Parse).ToList();
 
-    var stat = Stats.CalculateStats(condition.ToCharArray(), conditionGroups);
-    if (!stat.CheckStat())
-        throw new InvalidOperationException();
+
     // naive
-    var nbMatch = NbMatch(condition.ToCharArray(), conditionGroups[0], conditionGroups.Skip(1).ToList(), false, stat);
+    var nbMatch = NbMatchI(condition.ToCharArray(), conditionGroups[0], conditionGroups.Skip(1).ToList(), false);
     stopwatch.Stop();
-    Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed}  {line} ");
+    // Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed}  {line} ");
     return nbMatch;
 }).Sum();
 Console.WriteLine($"Part 1 {sumNb}");
 
+var stopwatch0 = Stopwatch.StartNew();
 var sumNbUnfold = File.ReadLines("input.txt")
-    .AsParallel().AsUnordered()
+  //  .AsParallel().AsUnordered()
     .Select((line, index) =>
     {
-        var stopwatch = Stopwatch.StartNew();
+
         var linePart = line.Split(' ');
         var condition0 = linePart[0];
         var conditionGroups0 = linePart[1].Split(",").Select(int.Parse).ToList();
@@ -83,19 +88,14 @@ var sumNbUnfold = File.ReadLines("input.txt")
         var condition = condition0 + '?' + condition0 + '?' + condition0 + '?' + condition0 + '?' + condition0;
         var conditionGroups = conditionGroups0.Concat(conditionGroups0).Concat(conditionGroups0).Concat(conditionGroups0).Concat(conditionGroups0).ToList();
 
-
-        var stat = Stats.CalculateStats(condition.ToCharArray(), conditionGroups);
-        if (!stat.CheckStat())
-            throw new InvalidOperationException();
-
         // naive
-        var nbMatch = NbMatch(condition.ToCharArray(), conditionGroups[0], conditionGroups.Skip(1).ToList(), false, stat);
+        var nbMatch = NbMatchI(condition.ToCharArray(), conditionGroups[0], conditionGroups.Skip(1).ToList(), false);
 
-        stopwatch.Stop();
-        Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed}  {line} ");
+        // Console.WriteLine($"({index:000}) {nbMatch,10}   {line} ");
         return nbMatch;
     }).Sum();
-Console.WriteLine($"Part 2 {sumNbUnfold}");
+stopwatch0.Stop();
+Console.WriteLine($"Part 2 {sumNbUnfold} {stopwatch0.Elapsed}");
 #endif
 
 
@@ -207,9 +207,6 @@ static long NbMatchNewCachedC(string chars, List<int> conditionGroups, Dictionar
     return nbMatch;
 }
 
-
-
-
 var sumNb2 = File.ReadLines("input.txt")
     .Select((line, index) =>
     {
@@ -221,14 +218,14 @@ var sumNb2 = File.ReadLines("input.txt")
         var nbMatch = NbMatchNewCached(condition, conditionGroups);
 
         stopwatch.Stop();
-        Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed}  {line} ");
+        // Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed}  {line} ");
         return nbMatch;
     }).Sum();
 Console.WriteLine($"Part 1 {sumNb2}");
 
 
-var sumNbUnfold = File.ReadLines("input.txt")
-    .AsParallel().AsUnordered()
+var stopwatch = Stopwatch.StartNew();
+var sumNbUnfoldN = File.ReadLines("input.txt")
     .Select((line, index) =>
     {
         var linePart = line.Split(' ');
@@ -238,12 +235,14 @@ var sumNbUnfold = File.ReadLines("input.txt")
         var condition = condition0 + '?' + condition0 + '?' + condition0 + '?' + condition0 + '?' + condition0;
         var conditionGroups = conditionGroups0.Concat(conditionGroups0).Concat(conditionGroups0).Concat(conditionGroups0).Concat(conditionGroups0).ToList();
 
-        var stopwatch = Stopwatch.StartNew();
+
         var nbMatch = NbMatchNewCached(condition, conditionGroups);
-        stopwatch.Stop();
-        Console.WriteLine($"({index:000}) {nbMatch,10} {stopwatch.Elapsed} {line} ");
+
+        //  Console.WriteLine($"({index:000}) {nbMatch,10} {line} ");
         return nbMatch;
     }).Sum();
-Console.WriteLine($"Part 2 {sumNbUnfold}");
+stopwatch.Stop();
+
+Console.WriteLine($"Part 2 {sumNbUnfoldN}   {stopwatch.Elapsed}");
 
 // Console.ReadLine();
