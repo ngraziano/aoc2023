7adb2f440b9254dfd174231ca2fd42ef2f66a588
Nicolas Graziano <nicolas.graziano@gmail.com>
2023-12-15 16:32:35 +0100

    Day 15 try pattern mathcing

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,57 +1,49 @@
 ﻿
-using System.ComponentModel;
 using System.Reflection.Emit;
+using System.Text.RegularExpressions;
 
-static byte Hash(string input)
-{
-
-    return input.Aggregate((byte)0, (hash, c) =>
-    {
-        int val = (hash + c) * 17;
-        return (byte)val;
-    });
-}
+static byte Hash(string input) =>
+    input.Aggregate((byte)0, (hash, c) => (byte)((hash + c) * 17));
 
 var sumOfHash = File.ReadLines("input.txt").First().Split(',')
                 .Select(Hash).Select(x => (int)x).Sum();
 
 Console.WriteLine($"Part 1 sum of hash = {sumOfHash}");
 
-
-var hashmap = Enumerable.Range(0, 256).Select((_) => new List<Slot>()).ToList();
+var hashmap = Enumerable.Range(0, 256).Select((_) => new List<(string Label, int Focal)>()).ToList();
 
 var line = File.ReadLines("input.txt").First();
-//var line = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 foreach (string intruction in line.Split(','))
 {
-    if (intruction.EndsWith('-'))
+    var (label, operation, focal) = intruction switch
     {
-        var label = intruction[..^1];
-        var box = hashmap[Hash(label)];
-        box.RemoveAll(e => e.Label == label);
-    } else if(intruction.Contains('='))
+        [.. var l, '-'] => (l, Operation.REMOVE, 0),
+        [.. var l, '=', var v] => (l, Operation.ASSIGN, v - '0'),
+        _ => ("", Operation.NULL, 0)
+    };
+
+    var box = hashmap[Hash(label)];
+    var slotIndex = box.FindIndex(e => e.Label == label);
+    switch (operation)
     {
-        var label = intruction.Split('=')[0];
-        var focal = int.Parse(intruction.Split("=")[1]);
-        var box = hashmap[Hash(label)];
-        
-        var place = box.Find(e => e.Label == label);
-        if(place is null)
-        {
-            box.Add(new Slot { Label = label, Focal = focal });
-        } else
-        {
-            place.Focal = focal;
-        }
+        case Operation.REMOVE when slotIndex >= 0:
+            box.RemoveAt(slotIndex);
+            break;
+        case Operation.ASSIGN when slotIndex >= 0:
+            box[slotIndex] = (label, focal);
+            break;
+        case Operation.ASSIGN:
+            box.Add((label, focal));
+            break;
     }
 }
 
 var sumFocus = hashmap.SelectMany((box, boxNumber) => box.Select((slot, slotNumber) => (boxNumber + 1) * (slotNumber + 1) * slot.Focal)).Sum();
 Console.WriteLine($"Part 2 {sumFocus}");
 
-
-public record Slot
+enum Operation
 {
-    public string Label { get; set; } = "";
-    public int Focal { get; set; }
-}
+    NULL,
+    REMOVE,
+    ASSIGN,
+};
