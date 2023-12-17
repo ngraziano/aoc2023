ab614a8e38cd08d4fd7397248281cde575c454bd
Nicolas Graziano <nicolas.graziano@gmail.com>
2023-12-17 18:33:02 +0100

    day 17 part 1

---
+++ day17/Program.cs
@@ -0,0 +1,120 @@
+﻿#if false
+const string file = "input0.txt";
+
+var board = File.ReadLines(file)
+    .Select(l => l.Select(c => c - '0').ToArray()).ToArray();
+
+
+
+
+
+(int X, int Y) maxPos = (board[0].Length - 1, board.Length - 1);
+
+
+
+
+
+// X=Y
+// get a trivial max to limit search
+var currentMinimal = Enumerable.Range(0, maxPos.X)
+    .Select(x => board[x][x + 1] + board[x + 1][x + 1]).Sum();
+ currentMinimal = 120;
+bool InBoard(int x, int y)
+{
+    return x >= 0 && x <= maxPos.X
+        && y >= 0 && y <= maxPos.Y;
+}
+
+
+IEnumerable<PathPos> Move(PathPos pos)
+{
+    var (newX, newY) = pos.Dir switch
+    {
+        Direction.N => (pos.X, pos.Y - 1),
+        Direction.S => (pos.X, pos.Y + 1),
+        Direction.E => (pos.X + 1, pos.Y),
+        Direction.W => (pos.X - 1, pos.Y),
+        _ => throw new NotImplementedException(),
+    };
+
+
+    if (!InBoard(newX, newY))
+    {
+        yield break;
+    }
+
+    if (pos.NbStraight < 2)
+    {
+        yield return new PathPos(newX, newY, pos.Dir, pos.NbStraight + 1);
+
+    }
+
+    yield return new PathPos(newX, newY, (Direction)(((int)pos.Dir + 1) % 4), 0);
+    yield return new PathPos(newX, newY, (Direction)(((int)pos.Dir + 3) % 4), 0);
+}
+
+
+var cache = new Dictionary<PathPos, int>();
+
+int CalculateMinToEndFrom(PathPos pos, HashSet<PathPos> history, int currentLost)
+{
+    if ((pos.X, pos.Y) == maxPos)
+        return board![pos.Y][pos.X];
+
+ //   if (currentLost > currentMinimal)
+ //       return -1;
+
+    if (cache!.TryGetValue(pos, out var cachedValue))
+    {
+        return cachedValue;
+    }
+
+    var value = Move(pos)
+        .Where(p=> !history.Contains(p))
+        .Select(p => {
+            var newLost = currentLost + board![p.Y][p.X];
+            // Console.WriteLine($" {p} => {currentLost} / { string.Join(",", history.Select(p => $"({p.X},{p.Y},{p.Dir})"))} "  );
+            Console.WriteLine($" {p} => {currentLost}");
+            var child = CalculateMinToEndFrom(p, [.. history, p], newLost);
+
+            if (child > 0) {
+                return board![p.Y][p.X] + child;
+            } else
+            {
+                return -1;
+            }
+    }
+    
+    ).Where(x=>x>0).DefaultIfEmpty(-1).Min();
+
+    if(value>0)
+    {
+        cache[pos] = value;
+        return value;
+
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+var min = Math.Min(
+    CalculateMinToEndFrom(new PathPos(0, 0, Direction.E, 0), [],0),
+    CalculateMinToEndFrom(new PathPos(0, 0, Direction.S, 0), [], 0)
+    );
+
+Console.WriteLine($"Part 1 {min}");
+
+
+enum Direction
+{
+    N = 0,
+    E,
+    S,
+    W
+}
+
+
+record PathPos(int X, int Y, Direction Dir, int NbStraight);
+#endif
---
+++ day17/Program.old.cs
@@ -0,0 +1,222 @@
+﻿#if true
+
+using System.Collections.Concurrent;
+
+const string file = "input.txt";
+
+var board = File.ReadLines(file)
+    .Select(l => l.Select(c => c - '0').ToArray()).ToArray();
+
+PathTesting.MaxPos = (board[0].Length - 1, board.Length - 1);
+
+// X=Y
+// get a max 
+var currentMinimal = Enumerable.Range(0, PathTesting.MaxPos.X)
+    .Select(x => board[x][x + 1] + board[x + 1][x + 1]).Sum();
+#if false
+var pathTotest = new List<PathTesting>()
+{
+    new()
+    {
+        Direction = Direction.E,
+        NbStraight = 0,
+        Pos = (0,0),
+    },
+    new()
+    {
+        Direction = Direction.S,
+        NbStraight = 0,
+        Pos = (0,0),
+  //      OldPos = []
+    }
+};
+
+var alreadyThere = new ConcurrentDictionary<(int X, int Y, Direction D, int NbStraight), int>();
+
+int i = 0;
+while (pathTotest.Count > 0)
+{
+    i++;
+    pathTotest = pathTotest.AsParallel().SelectMany(path => path.Move(board))
+        .OrderBy(p => p.HeatLoss)
+        .Where(p =>
+    {
+        if (alreadyThere.TryGetValue((p.Pos.X, p.Pos.Y, p.Direction, p.NbStraight), out var val))
+        {
+            if (val >= p.HeatLoss)
+            {
+                alreadyThere[(p.Pos.X, p.Pos.Y, p.Direction, p.NbStraight)] = p.HeatLoss;
+                return true;
+            }
+            return false;
+        }
+        alreadyThere[(p.Pos.X, p.Pos.Y, p.Direction, p.NbStraight)] = p.HeatLoss;
+
+        return true;
+    }).ToList();
+
+    currentMinimal = pathTotest.Where(e => e.Pos == PathTesting.MaxPos)
+                            .Select(e => e.HeatLoss)
+                            .DefaultIfEmpty(currentMinimal)
+                            .Min();
+    /*   foreach (var path in pathTotest.Where(p => p.Pos == PathTesting.MaxPos && p.HeatLoss == currentMinimal))
+       {
+           Console.WriteLine($"mini {path.HeatLoss} => {string.Join(" ", path.OldPos)}");
+
+           var boardTest = File.ReadLines(file).Select(l => l.ToArray()).ToArray();
+           foreach (var posd in path.OldPos)
+           {
+               boardTest[posd.Item1.Y][posd.Item1.X] = posd.Item2 switch
+               {
+                   Direction.E => '>',
+                   Direction.N => '^',
+                   Direction.S => 'v',
+                   Direction.W => 'W',
+                   _ => throw new NotImplementedException()
+               };
+           }
+           foreach (var l in boardTest)
+           {
+               Console.WriteLine(new string(l));
+           }
+       }
+    */
+    pathTotest = pathTotest.Where(e => e.Pos != PathTesting.MaxPos && e.HeatLoss < currentMinimal)
+                            .ToList();
+
+    var currentMinLoss = pathTotest.Select(e => e.HeatLoss).FirstOrDefault(currentMinimal);
+    var currentMaxLoss = pathTotest.Select(e => e.HeatLoss).LastOrDefault(currentMinimal);
+
+    Console.WriteLine($"[{i}] Nb test path {pathTotest.Count}, current minimal {currentMinimal} current path loss {currentMinLoss} {currentMaxLoss}");
+};
+#endif
+
+
+var pathTotest = new Stack<PathTesting>();
+pathTotest.Push(
+    new()
+    {
+        Direction = Direction.E,
+        NbStraight = 0,
+        Pos = (0, 0),
+    });
+pathTotest.Push(new()
+    {
+        Direction = Direction.S,
+        NbStraight = 0,
+        Pos = (0,0),
+  //      OldPos = []
+    }
+);
+int i = 0;
+
+var alreadyThere = new Dictionary<(int X, int Y, Direction D, int NbStraight), int>();
+
+while (pathTotest.TryPop(out var path))
+{
+    
+    if (path.HeatLoss > currentMinimal)
+        continue;
+
+    if (alreadyThere.TryGetValue((path.Pos.X, path.Pos.Y, path.Direction, path.NbStraight), out var lostSeen) 
+        && lostSeen <= path.HeatLoss)
+    {
+        continue;
+    }
+
+    alreadyThere[(path.Pos.X, path.Pos.Y, path.Direction, path.NbStraight)] = path.HeatLoss;
+
+    if (path.Pos == PathTesting.MaxPos && path.HeatLoss < currentMinimal)
+    {
+        currentMinimal = path.HeatLoss;
+    } 
+    else
+    {
+        i++;
+        if(i%100000==0)
+        Console.WriteLine($"{i} {pathTotest.Count}    =>  {path.HeatLoss} | {currentMinimal}");
+
+        foreach (var np in path.Move(board))
+        {
+            pathTotest.Push(np);
+        }
+    }
+}
+
+
+Console.WriteLine($"Part 1 {currentMinimal}");
+
+
+enum Direction
+{
+    N = 0,
+    E,
+    S,
+    W
+}
+
+struct PathTesting
+{
+    public static (int X, int Y) MaxPos { get; set; }
+    public (int X, int Y) Pos { get; init; }
+    public Direction Direction { get; init; }
+    public int NbStraight { get; init; }
+
+    //  public required List<((int X, int Y), Direction)> OldPos { get; init; }
+    public int HeatLoss { get; init; }
+
+    internal IEnumerable<PathTesting> Move(int[][] board)
+    {
+        var newPos = MovePos(Direction, Pos);
+        if (!InBoard(newPos))
+        {
+            yield break;
+        }
+
+        if (NbStraight < 2)
+        {
+            yield return new PathTesting()
+            {
+                Direction = Direction,
+                NbStraight = NbStraight + 1,
+                HeatLoss = HeatLoss + board[newPos.Y][newPos.X],
+                Pos = newPos,
+                //             OldPos = [.. OldPos, (newPos, Direction)],
+            };
+        }
+
+        yield return new PathTesting()
+        {
+            Direction = (Direction)(((int)Direction + 1) % 4),
+            NbStraight = 0,
+            HeatLoss = HeatLoss + board[newPos.Y][newPos.X],
+            Pos = newPos,
+            //        OldPos = [.. OldPos, (newPos, Direction)],
+        };
+        yield return new PathTesting()
+        {
+            Direction = (Direction)(((int)Direction + 3) % 4),
+            NbStraight = 0,
+            HeatLoss = HeatLoss + board[newPos.Y][newPos.X],
+            Pos = newPos,
+            //        OldPos = [.. OldPos, (newPos, Direction)],
+        };
+    }
+    private static (int X, int Y) MovePos(Direction direction, (int X, int Y) pos)
+    {
+        return direction switch
+        {
+            Direction.N => (pos.X, pos.Y - 1),
+            Direction.S => (pos.X, pos.Y + 1),
+            Direction.E => (pos.X + 1, pos.Y),
+            Direction.W => (pos.X - 1, pos.Y),
+            _ => throw new NotImplementedException(),
+        };
+    }
+    private static bool InBoard((int X, int Y) pos)
+    {
+        return pos.X >= 0 && pos.X <= MaxPos.X
+            && pos.Y >= 0 && pos.Y <= MaxPos.Y;
+    }
+}
+#endif
